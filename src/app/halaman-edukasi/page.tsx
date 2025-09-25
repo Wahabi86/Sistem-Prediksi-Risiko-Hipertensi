bacddcacfacc16fc58e55ca67fa14de0a85cd1b4
Wahabi86 <gustiabi97@gmail.com>
2025-09-25 14:12:15 +0800

    setup page and add navbar

---
+++ src/app/halaman-edukasi/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function EdukasiPage() {
+  return (
+    <div>
+      <h1 className="text-5xl text-center">Halaman Edukasi</h1>
+    </div>
+  );
+}
+
+export default EdukasiPage;
---
+++ src/app/halaman-riwayat/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function RiwayatPage() {
+  return (
+    <div>
+      <h1 className="text-5xl text-center">Halaman Riwayat</h1>
+    </div>
+  );
+}
+
+export default RiwayatPage;
