4ed7bc90759866aff727155e5da9d27d1a9f57df
Wahabi86 <gustiabi97@gmail.com>
2025-11-26 22:22:34 +0800

    update responsive untuk section tutorial di home

--- fe-hipertensi/src/components/ui/Tutorial.tsx
+++ fe-hipertensi/src/components/ui/Tutorial.tsx
@@ -5,14 +5,14 @@ import { caraPenggunaan } from "@/data/datas";
 export default function Tutorial() {
   return (
     <div className="container mx-auto mb-24 mt-24">
-      <h2 className="text-3xl font-bold mb-8">CARA MENGGUNAKAN</h2>
+      <h2 className="text-3xl font-bold mb-8 text-center">CARA MENGGUNAKAN</h2>
 
       <div className="grid grid-cols-1 md:grid-cols-2 gap-10 mb-8">
         {/* Tutorial 1 & 2 */}
         {caraPenggunaan.slice(0, 2).map((item, index) => (
-          <div key={index} className="bg-white rounded-3xl p-8 shadow-lg hover:shadow-xl transition-shadow duration-300 border border-gray-100">
-            <h3 className="text-2xl font-bold mb-4">{item.title}</h3>
-            <p className="text-xl leading-relaxed">{item.description}</p>
+          <div key={index} className="bg-white rounded-3xl p-6 sm:p-8 shadow-lg hover:shadow-xl transition-shadow duration-300 border border-gray-100">
+            <h3 className="text-xl sm:text-2xl font-bold mb-4">{item.title}</h3>
+            <p className="text-lg sm:text-xl leading-relaxed">{item.description}</p>
           </div>
         ))}
       </div>
